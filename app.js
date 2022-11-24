48c5264cb4ff951ee44d08f6bd757e17a4710491
Anastasia Fedorova <anfed722@gmail.com>
2022-11-24 17:03:36 +0400

    improve cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -7,44 +8,47 @@ const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
 
+
 const { PORT = 3000 } = process.env;
 const { DB_ADDRESS = 'mongodb://localhost:27017/moviesdb' } = process.env;
 
 const app = express();
 
-const allowedCors = [
-  'http://movies-explorer.anfed.nomoredomains.icu',
-  'https://movies-explorer.anfed.nomoredomains.icu',
-  'localhost:3000',
-];
+// const allowedCors = [
+//   'http://movies-explorer.anfed.nomoredomains.icu',
+//   'https://movies-explorer.anfed.nomoredomains.icu',
+//   'localhost:3000',
+// ];
 
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+app.use(cors());
 
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+// app.use((req, res, next) => {
+//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
 
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+//   // проверяем, что источник запроса есть среди разрешённых
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
 
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  // сохраняем список заголовков исходного запроса
-  const requestHeaders = req.headers['access-control-request-headers'];
+//   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
+//   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   // сохраняем список заголовков исходного запроса
+//   const requestHeaders = req.headers['access-control-request-headers'];
 
-  return next();
-});
+//   // Если это предварительный запрос, добавляем нужные заголовки
+//   if (method === 'OPTIONS') {
+//     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     // разрешаем кросс-доменные запросы с этими заголовками
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     // завершаем обработку запроса и возвращаем результат клиенту
+//     return res.end();
+//   }
+
+//   return next();
+// });
 
 app.use(requestLogger);
 
